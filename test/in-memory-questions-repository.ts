9f7bdb14566488b9e593edfdb0032f7414d6248e
INTLPiva <lucas.piva@gec.inatel.br>
2025-11-24 09:29:17 -0300

    feat: delete question use case

--- test/in-memory-questions-repository.ts
+++ test/in-memory-questions-repository.ts
@@ -4,17 +4,33 @@ import { Question } from "@/domain/forum/enterprise/entities/question";
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = [];
 
-  async findBySlug(slug: string) {
-    const quesiton = this.items.find((item) => item.slug.value === slug);
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id);
 
-    if (!quesiton) {
+    if (!question) {
       return null;
     }
 
-    return quesiton;
+    return question;
+  }
+
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug);
+
+    if (!question) {
+      return null;
+    }
+
+    return question;
   }
 
   async create(question: Question) {
     this.items.push(question);
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    this.items.splice(itemIndex, 1);
   }
 }
