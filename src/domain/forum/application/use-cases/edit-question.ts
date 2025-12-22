3536ba681d3d31dfb36a97d6bc1e7058d41cc13e
INTLPiva <lucas.piva@gec.inatel.br>
2025-12-22 10:56:34 -0300

    feat: edit question controller

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -7,6 +7,7 @@ import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { ResourceNotFoundError } from "../../../../core/errors/errors/resource-not-found-error";
 import { NotAllowedError } from "../../../../core/errors/errors/not-allowed-error";
+import { Injectable } from "@nestjs/common";
 
 interface EditQuestionUseCaseRequest {
   authorId: string;
@@ -23,6 +24,7 @@ type EditQuestionUseCaseResponse = Either<
   }
 >;
 
+@Injectable()
 export class EditQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
